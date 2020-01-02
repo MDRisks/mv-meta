7a9a60aed3d17a9472c2156cb516c006d69cdc76
Borys Lemeshko <lemeshko.borys@gmail.com>
2020-01-02 10:16:41 +0200

    typo

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,8 +8,8 @@ fi
 domains=(mv.starnavi.co)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="borys.lemeshko@starnavi.io" # Adding a valid address is strongly recommended
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision

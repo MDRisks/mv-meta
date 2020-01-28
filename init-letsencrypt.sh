08aacfc9abfd213453b7932c16a7b040d9ba9f00
Borys Lemeshko <lemeshko.borys@gmail.com>
2020-01-28 16:09:36 +0200

    updated for preview

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(mv.starnavi.co)
+domains=(mv.starnavi.co mdrisks.site www.mdrisks.site)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="borys.lemeshko@starnavi.io" # Adding a valid address is strongly recommended
+email="nicholas.petros@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
